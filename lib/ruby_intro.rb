5f7da41e00afdc867df525b714db84fb886fff39
EC2 Default User <ec2-user@ip-172-31-82-121.ec2.internal>
2020-02-01 05:50:06 +0000

    Pushing changes to ruby_intro.rb, succeed on all tests except the final part of BookInStock

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,84 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  @total = 0
+  arr.each do |x|
+    @total += x
+  end
+  return @total
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum(arr.max(2)) #used previous method I made, as well as array documentation
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  #idea, subtract n from all terms, then see if the resulting array contains
+  # any of the original values of arr.
+  if arr.length() <= 1 
+    return false
+  end
+  
+  arr.each do |x|
+    if arr.index(n-x) == arr.index(x) #check to see if index is the same, or
+      return false                    #if the number is half of n
+    end
+    if arr.index(n-x) != nil
+      return true
+    end 
+  end
+  
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  @hello_string =  "Hello, #{name}" #needs a definition, cant just use puts.
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  @consonants = "bcdfghjklmnpqrstvwxyz"
+  
+  if s.length < 1
+    return false
+  end
+  if @consonants.index(s[0]) != nil or @consonants.upcase.index(s[0]) != nil
+    return true
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  @num = s.to_i(2)
+  
+  if @num%4==0
+    return true
+  end
+  
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(i="0", p=1.0)
+    @isbn = i
+    @price = p
+    
+    raise ArgumentError, "Invalid isbn" unless i.length > 0
+    raise ArgumentError, "Price is 0" unless p > 0
+  end
+  def isbn
+    @isbn
+  end
+  def price
+    @price
+  end
+  def isbn= i
+    @isbn = i
+  end
+  def price= p
+    @price = p
+  end
+  
+  
 end
